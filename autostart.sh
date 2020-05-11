2f8bd3738f9d3c80e2f4568cbbed275d30d33ea4
Alex Cline <rev.a.r.cline@gmail.com>
2020-05-11 11:22:26 -0500

    starting out

---
+++ autostart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+run "nm-applet"
+run "volumeicon"
+run "pamac-tray"
+run "blueberry-tray"
+run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+run "numlockx on"
+run "nitrogen --restore"
+run "vgrive"
+run "xfce4-power-manager"
